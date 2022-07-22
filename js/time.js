2b2b368b6189d4f69ea5f0b605a22c4639def9d7
LueKely <luekely43110@gmail.com>
2022-07-22 11:39:43 +0800

    updated time and added icons

---
+++ js/time.js
@@ -0,0 +1,40 @@
+const dayTime = document.querySelector('.time1');
+const currentTime = document.querySelector('.time__currently');
+const amPm = document.getElementById('am-pm');
+function getDayTime() {
+	let date = new Date();
+	let hours = date.getHours();
+	if (hours >= 6 && hours < 12) {
+		dayTime.textContent = 'Morning';
+	} else if (hours >= 12 && hours < 17) {
+		dayTime.textContent = 'Afternoon';
+	} else if (hours >= 17 && hours < 22) {
+		dayTime.textContent = 'evening';
+	} else if (hours >= 22 || hours <= 6) {
+		dayTime.textContent = 'night';
+	} else {
+		dayTime.textContent = 'error';
+	}
+}
+function getAmPm() {
+	let date = new Date();
+	let hours = date.getHours();
+
+	hours >= 12 ? (amPm.textContent = ' pm') : (amPm.textContent = ' am');
+}
+function getHourAndMinutes() {
+	let date = new Date();
+	let minutes = date.getMinutes();
+	let hours = date.getHours();
+
+	hours > 12 ? (hours = hours - 12) : (hours = hours);
+	minutes < 10 ? (minutes = '0' + minutes) : (minutes = minutes);
+	currentTime.textContent = hours + ':' + minutes;
+}
+function getTime() {
+	getAmPm();
+	getDayTime();
+	getHourAndMinutes();
+	setTimeout(getTime, 1000);
+}
+getTime();
